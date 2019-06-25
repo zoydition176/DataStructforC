e3162cad9e581a3f8081c79a51be9249c05bdf17
zoydition176 <zdx175851129@gmail.com>
2019-06-26 00:24:06 +0800

    1

--- sqList/sql.cpp
+++ sqList/sql.cpp
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
-typedef struct sqList
+struct sqList
 {
     int data[100];
     int length;
 };
 
+    // int findElement(sqList L,int x){
+
+    // };
+
 int main()
 {
-    printf("title : %s\nauthor: %s\nsubject: %s\nbook_id: %d\n", book.title, book.author, book.subject, book.book_id);
+    sqList L;
+    L.length = 20;
+    L.data[100] = {20,20,20,10,5};
+    printf("length is :%d", L.length);
 }
