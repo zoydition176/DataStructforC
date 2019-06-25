a70dd67b3fa699c6048017460b108646e78be37c
zoydition176 <zdx175851129@gmail.com>
2019-06-25 23:49:29 +0800

    1

---
+++ sqList/sql.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+typedef struct sqList
+{
+    int data[100];
+    int length;
+};
+
+int main()
+{
+    printf("title : %s\nauthor: %s\nsubject: %s\nbook_id: %d\n", book.title, book.author, book.subject, book.book_id);
+}
