7566063f5fd5dc13fd7ecf0ed47373907772073b
zoydition176 <zdx175851129@gmail.com>
2019-06-27 23:14:16 +0800

    1

--- sqList/sql.cpp
+++ sqList/sql.cpp
@@ -1,21 +1,34 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 struct sqList
 {
-    int data[100];
+    int data[20];
     int length;
 };
 
-    // int findElement(sqList L,int x){
+void sqListFilling(sqList k)
+{
+    int n;
+    if(k.length>0){//非空顺序表
+        for(int i=0;i<k.length;i++){
+            n = 1 + (int)(rand()%10);
+            k.data[i] = n;
+        }
+    }
 
-    // };
-
+};
 int main()
 {
+    int test;
     sqList L;
     L.length = 20;
-    L.data[0] = {20};
-    L.data[1] = {20};
-    L.data[2] = {20};
-    printf("length is :%d", L.length);
+    // test = rand()%10;
+    // printf("%d\n", test);
+    void sqListFilling(sqList L);
+    printf("%d\n", L.data[0]);
+    // for (int q = 0; q <= L.length; q++)
+    // {
+    //     printf("%d\n", L.data[q]);
+    // }
 }
