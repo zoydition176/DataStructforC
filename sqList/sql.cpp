63738307b4afc72034c03e2634ee3292a4edb4db
zoydition176 <zdx175851129@gmail.com>
2019-06-29 23:49:11 +0800

    创建顺序表

--- sqList/sql.cpp
+++ sqList/sql.cpp
@@ -4,43 +4,28 @@
 struct sqList
 {
     // int *element;
-    int data[20];
+    int data[10];
     int length;
 };
 
-void sqListFilling(sqList *&k){
-    int n;
-    k = (sqList *)malloc(sizeof(sqList));
-    if(k->length>0){//非空顺序表
-        for (int i = 0; i < k->length; i++)
-        {
-            n = 1 + (int)(rand()%10);
-            k->data[i] = n;
+void sqListFilling(sqList k)
+{
+    int n = 0;
+    if(k.length>0){
+        for(int i=0;i<k.length;i++){
+            k.data[i] = i;
         }
     }
 
 };
-
-// int initSqlist(){
-//     L->elem = (ElemType *)malloc(sizeof(ElemType));
-//     if (!L->elem)
-//         return ERROR;
-//     L->length = 0;
-//     return 1;
-// };
 int main()
 {
-    sqList *L;
+    int test;
+    sqList L;
+    L.length = 20;
     sqListFilling(L);
-    // int test;
-    // sqList L;
-    // L.length = 20;
-    // test = rand()%10;
-    // printf("%d\n", test);
-    // void sqListFilling(sqList L);
-    // printf("%d\n", L.data[0]);
-    for (int q = 0; q <= L->length; q++)
+    for (int q = 0; q <= L.length; q++)
     {
-        printf("%d\n", L->data[q]);
+        printf("%d\n", L.data[q]);
     }
 }
