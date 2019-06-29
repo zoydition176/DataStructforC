e9b3fbfd065546adbe65043b27a1e406bc2ca9f8
zoydition176 <zdx175851129@gmail.com>
2019-06-29 23:44:16 +0800

    创建顺序表遇到的问题

--- sqList/sql.cpp
+++ sqList/sql.cpp
@@ -3,26 +3,35 @@
 #include <time.h>
 struct sqList
 {
-    int *element;
+    // int *element;
     int data[20];
     int length;
 };
 
-void sqListFilling(sqList k)
-{
-    // int n;
-    // if(k.length>0){//非空顺序表
-    //     for(int i=0;i<k.length;i++){
-    //         n = 1 + (int)(rand()%10);
-    //         k.data[i] = n;
-    //     }
-    // }
+void sqListFilling(sqList *&k){
+    int n;
+    k = (sqList *)malloc(sizeof(sqList));
+    if(k->length>0){//非空顺序表
+        for (int i = 0; i < k->length; i++)
+        {
+            n = 1 + (int)(rand()%10);
+            k->data[i] = n;
+        }
+    }
 
 };
 
-
+// int initSqlist(){
+//     L->elem = (ElemType *)malloc(sizeof(ElemType));
+//     if (!L->elem)
+//         return ERROR;
+//     L->length = 0;
+//     return 1;
+// };
 int main()
 {
+    sqList *L;
+    sqListFilling(L);
     // int test;
     // sqList L;
     // L.length = 20;
@@ -30,8 +39,8 @@ int main()
     // printf("%d\n", test);
     // void sqListFilling(sqList L);
     // printf("%d\n", L.data[0]);
-    // for (int q = 0; q <= L.length; q++)
-    // {
-    //     printf("%d\n", L.data[q]);
-    // }
+    for (int q = 0; q <= L->length; q++)
+    {
+        printf("%d\n", L->data[q]);
+    }
 }
