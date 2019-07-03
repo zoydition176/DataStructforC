226b0e6ffad66deaa8a339530839316d8cc3b960
zoydition176 <zdx175851129@gmail.com>
2019-07-03 23:01:50 +0800

    1

--- sqList/sql.cpp
+++ sqList/sql.cpp
@@ -1,30 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+/* 常量定义 */ 
+#define MAXSIZE 100
+
+
 struct sqList
 {
     // int *element;
-    int data[10];
+    int data[MAXSIZE];
     int length;
 };
 
-void sqListFilling(sqList *&k)
+// 初始化一个随机的顺序表，最大表长100
+void sqListFilling(sqList *&k,int q)
 {
     int n = 0;
+    k->length = q;
     srand((unsigned)time(NULL));
-    if(k->length>0){
+    if (q > 0)
+    {
         for (int i = 0; i < k->length; i++)
         {
-            n = rand()%20;
+            n = rand()%30;
             k->data[i] = n;
         }
     }
 };
+
+// 顺序表元素获取
+
 int main()
 {
     sqList *L = (sqList *)malloc(sizeof(sqList));
-    L->length = 20;
-    sqListFilling(L);
+    sqListFilling(L,10);
     for (int i = 0; i < L->length; i++)
     {
         printf("%d\n", L->data[i]);
