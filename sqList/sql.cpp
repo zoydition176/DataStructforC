a4b693e66c0007cf1ec11d006ec565dfa6d5d712
zoydition176 <zdx175851129@gmail.com>
2019-07-07 23:31:48 +0800

    1

--- sqList/sql.cpp
+++ sqList/sql.cpp
@@ -4,7 +4,8 @@
 
 /* 常量定义 */ 
 #define MAXSIZE 100
-
+#define DONE 1
+#define ERROR 0
 
 struct sqList
 {
@@ -29,17 +30,32 @@ void sqListFilling(sqList *&k,int q)
     }
 };
 
-// 顺序表元素获取
-int getElement(int i){
-    int k; 
+// 顺序表对应下标元素获取
+int findElement(sqList *k, int i)
+{
+    if(i>=0 && i<=k->length){
+        printf("THE ELEMENT YOU FIND IS %d\n", k->data[i]);
+        return DONE;
+    }else{
+        return ERROR;
+    }
 };
 
 int main()
 {
+    int start = DONE;
     sqList *L = (sqList *)malloc(sizeof(sqList));
     sqListFilling(L,10);
-    for (int i = 0; i < L->length; i++)
+    // for (int i = 0; i < L->length; i++)
+    // {
+    //     printf("%d\n", L->data[i]);
+    // }
+    while (start)
     {
-        printf("%d\n", L->data[i]);
+        printf("*****欢迎使用c与数据结构第一章线性表操作演示程序*****\n");
+        printf("*****请选择操作*****\n");
+        printf("*****按 1 :创建顺序表*****\n");
+        printf("*****按 1 :创建顺序表*****\n");
     }
+    
 }
