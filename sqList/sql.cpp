67ae6c26b50ccbf628d6f592c5c65a2771f02aec
zoydition176 <zdx175851129@gmail.com>
2019-07-12 23:42:07 +0800

    ss

--- sqList/sql.cpp
+++ sqList/sql.cpp
@@ -34,8 +34,8 @@ void sqListFilling(sqList *&k,int q)
 int findElement(sqList *k, int i)
 {
     if(i>=0 && i<=k->length){
-        printf("THE ELEMENT YOU FIND IS %d\n", k->data[i]);
-        return DONE;
+        // printf("THE ELEMENT YOU FIND IS %d\n", k->data[i]);
+        return k->data[i];
     }else{
         return ERROR;
     }
@@ -56,20 +56,38 @@ int main()
     {
         printf("*****欢迎使用c与数据结构第一章线性表操作演示程序*****\n");
         printf("*****请选择操作*****\n");
-        printf("*****按 1 :创建顺序表*****\n");
-        printf("*****按 2 :创建顺序表*****\n");
+        printf("*****按 1 :初始化顺序表*****\n");
+        printf("*****按 2 :获取下标对应顺序表元素*****\n");
         printf("*****按 9： 退出顺序表操作演示程序*****\n");
         scanf("%d", &choice);
         switch (choice)
         {
             case 1:
                 int name;
-                printf("您选择了1号操作:创建顺序表\n");
+                printf("您选择了1号操作:初始化顺序表\n");
                 printf("请输入顺序表的表长：\n");
                 scanf("%d", &name);
                 // sqList *L = (sqList *)malloc(sizeof(sqList));
-                sqListFilling(L, name);
-                printf("创建顺序表成功!\n");
+                sqListFilling(L, name);       
+                for (int i=0;i<=L->length;i++)
+                {
+                    printf("| %d |", L->data[i]);
+                };
+                printf("长度为 %d 的顺序表初始化成功!\n", name);
+                system("pause");
+                break;
+            case 2:
+                int num,res;
+                printf("您选择了2号操作:获取顺序表元素\n");
+                printf("请输入下标：\n");
+                scanf("%d", &num);
+                res = findElement(L, num);
+                if (res)
+                {
+                    printf("下标%d的元素为：%d \n", num, res);
+                }else{
+                    printf("输入值不合法!\n");
+                }
                 system("pause");
                 break;
             case 9:
