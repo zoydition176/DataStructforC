46297f1f126393b7c32f7c58d0771b5f644c90a4
zoydition176 <zdx175851129@gmail.com>
2019-08-01 23:24:09 +0800

    栈

--- queue/sqstack.c
+++ queue/sqstack.c
@@ -13,16 +13,41 @@ typedef struct sqstk
 // cpp特性是引用类型可以当函数形参传递，c语言不支持，只能用指针做形参。调用的时候用引用型做实参
 void initsqs(sqstk *st)
 {
-    st.top=-1;
+    st->top=0;
 };
 
-int initSqk(){
-
+// 判断栈的状态,普通类型的参数
+int isEmpty(sqstk st){
+    if(st.top == 0){
+        return 0;
+    }
+    else if (st.top == MAXSIZE)
+    {
+        return 2;
+    }else{
+        return 1;
+    }
 };
+
+// 进栈，实参为引用型
+int pushStack(sqstk *st,int n)
+{
+    int k = isEmpty(*st);
+    if(k==2){
+        return 0;
+    }else{
+        st->data[st->top]==n;
+        (st->top)++;
+        return 1;
+    }
+}
+
+
+
 
 int main()
 {
     sqstk a;
     // 定义一个栈
-    a.length = 0;
+    a.top = 0;
 }
