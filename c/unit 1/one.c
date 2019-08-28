88a5477781e7dd44efa077aa5b4e30ac42dd403f
zoydition176 <zdx175851129@gmail.com>
2019-08-28 23:38:04 +0800

    1

---
+++ c/unit 1/one.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXSIZE 100
+#define DONE 1
+#define ERROR 0
+
+int main()
+{
+    sqQueue *a = (sqQueue *)malloc(sizeof(sqQueue));
+    initQueue(a); //我叼你妈的总算对了
+    // 定义一个栈
+    isQueueEmpty(*a);
+}
