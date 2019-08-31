a0b902a4c4eeeea080904eefaaf60aa549e333b6
zoydition176 <zdx175851129@gmail.com>
2019-08-31 23:44:32 +0800

    1

--- c/unit 1/one.c
+++ c/unit 1/one.c
@@ -3,8 +3,30 @@
 #define MAXSIZE 100
 #define DONE 1
 #define ERROR 0
-// 百鸡问题
+// 计算阶乘
+int factorial(int n){
+    int res = 1;
+    if(n == 0){
+        return ERROR;
+    }else{
+        for (int i = 1; i <= n; i++)
+        {
+            res = res * i;
+        }
+        return res;
+    }
+}
+
 int main()
 {
-    //
-}
+    int num;
+    printf("1.《计算阶乘》\n请输入一个小于30的数字：");
+    scanf("%d", &num);
+    if (factorial(num))
+    {
+        printf("程序执行成功！计算结果为:%d\n", factorial(num));
+    }else{
+        printf("参数错误");
+    }
+    system("pause");
+};
