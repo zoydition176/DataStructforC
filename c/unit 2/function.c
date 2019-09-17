7c3a6b9b40d3d6361a8180b694b2f89fc97581b8
zoydition176 <zdx175851129@gmail.com>
2019-09-17 23:51:33 +0800

    1

--- c/unit 2/function.c
+++ c/unit 2/function.c
@@ -12,28 +12,29 @@ typedef struct sqList
 
 void reverse(sqList *q)
 {
-    // printf("%d,", q->data[0]);
     int n;
-    for(int i=0;i<(q->length)/2;i++){
-        n = q->data[q->length-1-i];
-        q->data[q->length-i] = q->data[i];
+    int k = q->length-1;
+    for (int i = 0; i < (q->length)/2; i++)
+    {
+        /* code */
+        n = q->data[k - i];
+        q->data[k - i] = q->data[i];
         q->data[i] = n;
-        printf("%d,", q->data[i]);
-        n = 0;
-    }   
+    }
+    for(int j = 0;j<q->length;j++){
+        printf("%d,", q->data[j]);
+    }
 }
 
 int main()
 {
     sqList *k = (sqList *)malloc(sizeof(sqList));
-    k->length = 10;
-    // k->data[0] = 1;
+    k->length = 19;
     for (int i = 0; i < k->length; i++)
     {
         k->data[i] = i;
         printf("%d,", k->data[i]);
     }
-    // printf("%d,", k->data[0]);
     printf("\n");
     reverse(k);
     return 0;
