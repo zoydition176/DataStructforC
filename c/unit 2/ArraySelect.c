8f631f172786c206dd05b6b742096702e7aecdb5
zoydition176 <zdx175851129@gmail.com>
2019-09-20 00:15:03 +0800

    1

--- c/unit 2/ArraySelect.c
+++ c/unit 2/ArraySelect.c
@@ -4,6 +4,16 @@
 #define DONE 1
 #define ERROR 0
 
+// 线性表
+typedef struct sqlist
+{
+    int data[MAXSIZE];
+    int length;
+} sqlist;
+
+// 从数组中选出最大和最小的两个数
+void initArray(){}
+
 int main(){
     int a;
     scanf("%d",&a);
