d7af4bd44da1e3e9918f5aee6a1efb3ccd29037a
zoydition176 <zdx175851129@gmail.com>
2019-09-26 00:22:00 +0800

    1

--- c/unit 2/ArraySelect.c
+++ c/unit 2/ArraySelect.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 #define MAXSIZE 100
 #define DONE 1
 #define ERROR 0
@@ -11,14 +13,53 @@ typedef struct sqlist
     int length;
 } sqlist;
 
+void printfSql(sqlist q);
+void initArray(sqlist *q, int n);
+
+int main()
+{
+    // int a;
+    // scanf("%d",&a);
+    sqlist *a = (sqlist *)malloc(sizeof(sqlist));
+    initArray(a, 10);
+    // printfSql(*a);
+}
 // 从数组中选出最大和最小的两个数
 
 // 初始化顺序表
-void initArray(sqlist *q,int n){
-    int a;
+void initArray(sqlist *q, int n)
+{
+    int a = 0;
+    q->length = n;
+    memset(q->data, 0, sizeof(q->data)); // 清空原有数组
+    srand((unsigned)time(NULL));         // 初始化时间种子
+    if (n > 0)
+    {
+        for (int i = 0; i < q->length; i++)
+        {
+            a = rand() % 30;
+            q->data[i] = a;
+        }
+    }
+    printfSql(*q);
 }
 
-int main(){
-    int a;
-    scanf("%d",&a);
+// 打印顺序表
+void printfSql(sqlist q)
+{
+    if (q.length > 0)
+    {
+        for (int i = 0; i < q.length; i++)
+        {
+            if (i == q.length - 1)
+            {
+                printf("%d\n", q.data[i]);
+            }
+            else
+            {
+                printf("%d,", q.data[i]);
+            }
+        }
+    }
 }
+
