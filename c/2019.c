3dbd6c207da3647dec85ddc3c7d7084e244c36aa
zoydition176 <zdx175851129@gmail.com>
2019-11-05 23:52:43 +0800

    1

--- c/2019.c
+++ c/2019.c
@@ -7,21 +7,37 @@
 #define ERROR 0
 int main()
 {
-    char str[81],*p;
-    int i;
-    for(i=0;i<80;i++){
-        str[i]=getchar();
-        if(str[i]=='\n'){
-            break;
+    // char str[81],*p;
+    // int i;
+    // for(i=0;i<80;i++){
+    //     str[i]=getchar();
+    //     if(str[i]=='\n'){
+    //         break;
+    //     }
+    // }
+    // str[i]='\0';
+    // p = str;
+    
+    // // p = str;
+    // while (*p)
+    // {
+    //     /* code */
+    //     putchar(*(p++));
+    // }
+
+    int i = 1;
+    float sum = 0;
+
+    while (i <= 100)
+    {
+        if (i % 2 == 0)
+        {
+            sum -= 1 / (i++ * 1.0);
+        }
+        else
+        {
+            sum += 1 / (i++ * 1.0);
         }
     }
-    str[i]='\0';
-    p = str;
-    
-    // p = str;
-    while (*p)
-    {
-        /* code */
-        putchar(*(p++));
-    }
+    printf("%f\n", sum);
 }
