e185438d4df1f7e43a1a17e29abad026ef044cf2
zoydition176 <zdx175851129@gmail.com>
2019-12-05 16:42:23 +0800

    1

--- c/2016.c
+++ c/2016.c
@@ -5,11 +5,31 @@
 #define MAXSIZE 100
 #define DONE 1
 #define ERROR 0
+double glg(double c);
 int main()
 {
-    int x,y;
-    scanf("%d",&x);
-    x = x>12?x+10:x-12;
-    printf("%d\n",y);
+    // int x,y;
+    // scanf("%d",&x);
+    // x = x>12?x+10:x-12;
+    // double p = 1e-4;
+    // printf("%d\n",y);
+    // printf("%Lf\n",p);
+    printf("%f", glg(1e-4));
     return 0;
+}
+double glg(double c){
+    double t = 0,item = 1;
+    int i = 1;
+    while (item>=c)
+    {
+        /* code */
+        item = 1.0 / (1+(i-1)*2);
+        if(i%2==0){        
+            t = t - item;
+        }else{
+            t = t + item;
+        }
+        i++;
+    }
+    return t*4;
 }
