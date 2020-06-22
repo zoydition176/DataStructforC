5e98ac250bda1e52a6723c8c6bbc6cfd37a8dd98
zoydition176 <zdx175851129@gmail.com>
2020-06-22 21:11:48 +0800

    1

--- leetcode/leetcode3.c
+++ leetcode/leetcode3.c
@@ -79,3 +79,26 @@ double findMedianSortedArrays(int *nums1, int nums1Size, int *nums2, int nums2Si
         return ((double)a[ave] + (double)a[ave - 1]) / 2;
     }   
 }
+
+/*
+给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+
+注意:
+
+假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+
+*/
+int reverse(int x)
+{
+    long result = 0;
+    while (x != 0)
+    {
+        result = result * 10 + x % 10;
+        x = x / 10;
+    }
+    if (result != (int)result)
+    {
+        return 0;
+    }
+    return (int)result;
+}
